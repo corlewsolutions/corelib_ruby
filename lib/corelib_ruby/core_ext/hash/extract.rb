9828fc4f6302c254dbd81254bc6a20026d29dd99
Robert Soly <robert.m.soly@uscis.dhs.gov>
2016-05-06 16:43:04 -0400

    Simplify Hash#co_extract

--- lib/corelib_ruby/core_ext/hash/extract.rb
+++ lib/corelib_ruby/core_ext/hash/extract.rb
@@ -4,7 +4,7 @@ class Hash
   #
   # If key exists in the hash, it's value will be returned.
   # If key doesn't exist, and optional default value will be returned
-  # If key doesn't exist and not default is specified, a KeyError is raised
+  # If key doesn't exist and no default is specified, a KeyError is raised
   # If key doesn't exists, there is no default, but a block is given, then
   # that block will be executed with the key.
   def co_extract(*args)
@@ -12,12 +12,10 @@ class Hash
     fail ArgumentError, "wrong number of arguments #{arg_length} for 1..2" if arg_length == 0 || arg_length > 2
     key = args[0]
 
-    if key?(key)
-      val = self[key]
-      delete(key)
-      return val
-    end
+    # Just return the value if the key exists.
+    return delete(key) if key?(key)
 
+    # The key doesn't exist.  If the user gave us a block, yield to it and return the value from the block.
     return yield key if block_given?
     (args.length == 2) ? args[1] : fail(KeyError, "key not found #{key.inspect}", caller)
   end
