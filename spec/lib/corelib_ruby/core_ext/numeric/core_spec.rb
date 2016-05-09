0525403391c2557fc7ea4f07d7aec4e4702d84ec
Robert Soly <robert.m.soly@uscis.dhs.gov>
2016-05-09 17:44:12 -0400

    Add #cl_to_yes_no test for Float

--- spec/lib/corelib_ruby/core_ext/numeric/core_spec.rb
+++ spec/lib/corelib_ruby/core_ext/numeric/core_spec.rb
@@ -1,12 +1,15 @@
-RSpec.describe NilClass do
+RSpec.describe Numeric do
   describe "#cl_to_yes_no" do
     it 'works' do
       expect(1.cl_to_yes_no).to eq("Yes")
+      expect(1.0.cl_to_yes_no).to eq("Yes")
       expect(0.cl_to_yes_no).to eq("No")
+      expect(0.0.cl_to_yes_no).to eq("No")
     end
 
     it 'is aliased' do
-      expect(1._to_yes_no).to eq("Yes")
+      expect(1._to_yes_no).to eq("Yes") # Test on Fixnum
+      expect(1.0._to_yes_no).to eq("Yes") # Test on Float
     end
   end
 end
